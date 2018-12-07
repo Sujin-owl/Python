dbb766f4b688552df456687bd6f878251f8f5876
Sujin Guo <sujinrice@gmail.com>
2018-12-07 12:08:18 -0600

    guo

---
+++ PyPoll/main.py
@@ -0,0 +1,40 @@
+import os
+import csv
+
+election_csv_path = os.path.join("election_data.csv")
+
+
+print("Election Analysis")
+print("-"*50)
+
+
+total_votes = 0
+candidates_votes = {}
+votes_percent = {}
+Base = 0
+with open(election_csv_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    for row in csvreader:
+        total_votes += 1
+        if row[2] in candidates_votes.keys():
+            candidates_votes[row[2]] += 1
+        else:
+            candidates_votes[row[2]] = 1
+
+
+for key,value in candidates_votes.items():
+    percent = round((int(value)/int(total_votes))*100,3)
+    votes_percent[key] = percent
+    if value > Base:
+        Winner = key
+        Base = value
+    
+print(f"Total Votes: {total_votes}")
+print("-"*50)
+for key,value in candidates_votes.items():
+    print(f"{key}: {votes_percent[key]}00% ({candidates_votes[key]})")
+print("-"*50)
+print(f"Winner: {Winner}")
+print("-"*50)
+
