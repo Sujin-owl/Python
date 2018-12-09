5bdaf9acef9f55c60f064396488ba93867100b6e
Sujin Guo <sujinrice@gmail.com>
2018-12-08 21:46:01 -0600

    guo

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,32 +18,37 @@ with open(budget_csv_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 #Skip the head
     header = next(csvreader)
-#for loop
+#Iterate through the row in csv file
     for row in csvreader:
-# total month 
+#Calculate the total month 
         total_months += 1
-# total revenue
+#Calculate the total revenue
         total_revenue += int(row[1])
-# revenue change
+#Calculate the revenue change
         revenue_change = int(row[1])-prev_revenue
         prev_revenue = int(row[1])
+#Update the revenue change list
         revenue_change_list += [revenue_change]
-# add date and revenue change to the definded list
+#Add revenue change and corresponding date to the definded list
         date.append(row[0])
         revenuechange.append([revenue_change])
     length = int(len(revenue_change_list)-1)
+#Calculate the average change
     average_change = round(sum(revenue_change_list[1:])/length,2)
+#Calculate the greateat increase and greatest decrease
     greatest_increase = max(revenue_change_list)
     greatest_decrease = min(revenue_change_list)
+#Find out the index of the max and min change
     index1 = revenue_change_list.index(max(revenue_change_list))
     index2 = revenue_change_list.index(min(revenue_change_list))
     
-    
+#print the analysis to the terminal  
     print(f"Total Months: {total_months}")
     print(f"Total: ${total_revenue}")
     print(f"Average Change: ${average_change}")
     print(f"Greatest Increase in Profits: {date[index1]} (${greatest_increase})")
     print(f"Greatest Decrease in Profits: {date[index2]} (${greatest_decrease})")
+#Open and write the results to txt file
 with open(result_txt_path, mode='w', newline='') as summary_txt:
     writer = csv.writer(summary_txt)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,35 +1,43 @@
+#import dependencies
 import os
 import csv
-
+# set the data path and export file name
 election_csv_path = os.path.join("election_data.csv")
 file_output = "results.txt"
 
 print("Election Analysis")
 print("-"*50)
 
-
+#set variables
 total_votes = 0
 candidates_votes = {}
 votes_percent = {}
 Base = 0
+#open the csv file
 with open(election_csv_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
+#iterate through the row in csv file
     for row in csvreader:
+#calculate the total votes
         total_votes += 1
+#put the candidates' name and count their votes in the dictionary
         if row[2] in candidates_votes.keys():
             candidates_votes[row[2]] += 1
         else:
             candidates_votes[row[2]] = 1
 
-
+#iterate through the key and corresponding value in the dictionary
 for key,value in candidates_votes.items():
+#calculate the vote% 
     percent = round((int(value)/int(total_votes))*100,3)
+#put key and value in votes_percent dictionary
     votes_percent[key] = percent
+#find out the winner
     if value > Base:
         Winner = key
         Base = value
-    
+#print the analysis to the terminal   
 print(f"Total Votes: {total_votes}")
 print("-"*50)
 for key,value in candidates_votes.items():
@@ -37,7 +45,7 @@ for key,value in candidates_votes.items():
 print("-"*50)
 print(f"Winner: {Winner}")
 print("-"*50)
-
+# open and export the text file with the results
 with open(file_output, 'w') as file:
     file.write("Election Results \n")
     file.write("------------------------------------- \n")
